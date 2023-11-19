458a9d8933fe198cff86a12d6d790d9af23f04a9
valsmanol <valsmanol@gmail.com>
2023-11-19 21:08:26 +0200

    import from camera and galley access

--- app/src/main/java/com/example/worldwanderers/InportPage.java
+++ app/src/main/java/com/example/worldwanderers/InportPage.java
@@ -2,6 +2,7 @@ package com.example.worldwanderers;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -22,10 +23,17 @@ public class InportPage extends AppCompatActivity
 
     @SuppressLint("MissingInflatedId")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.inport);
+
+
+
+        Button camera_logo=findViewById(R.id.camera_logo);
+        ImageView image_from_gallery=findViewById(R.id.image_from_gallery);
+
 
 
 // arrow go back to main board
@@ -43,25 +51,70 @@ public class InportPage extends AppCompatActivity
 
         });
 
+
+
+
+        // gallery import
+
         Button blue_gallery_logo=findViewById(R.id.blue_gallery_logo);
         blue_gallery_logo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intent,3);
+                startActivityForResult(intent,1);
 
             }
         });
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        //camera import
+        camera_logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent image_from_gallery=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(image_from_gallery,2);
+
+            }
+        });
+
+    }
+   /* @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode==RESULT_OK && data!= null){
+        if(resultCode==RESULT_OK && data!= null)
+        {
             Uri selectedImage=data.getData();
             ImageView image_from_gallery=findViewById(R.id.image_from_gallery);
             image_from_gallery.setImageURI(selectedImage);
+        }
+    }*/
 
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ImageView image_from_gallery=findViewById(R.id.image_from_gallery);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 2) {
+                Bitmap img = (Bitmap) (data.getExtras().get("data"));
+
+                // Check if img is not null before setting it
+                if (img != null) {
+                    image_from_gallery.setImageBitmap(img);
+                } else {
+                    // Handle the case where the Bitmap is null
+                }
+            } else if (requestCode == 1 && data != null) {
+                Uri selectedImage = data.getData();
+                image_from_gallery.setImageURI(selectedImage);
+            }
         }
     }
+
+
+
 }
