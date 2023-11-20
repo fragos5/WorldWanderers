57d3891256dd1b20a9c1422918bc373a94bdd5a8
pavlgkai <gpaulos2002@gmail.com>
2023-11-20 17:37:32 +0200

    buttons gia gallery

--- app/src/main/java/com/example/worldwanderers/GalleryPage.java
+++ app/src/main/java/com/example/worldwanderers/GalleryPage.java
@@ -21,6 +21,25 @@ public class GalleryPage extends AppCompatActivity {
 
         arrowBack=findViewById(R.id.arrowBack);
 
+        //buttons sto gallery
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+        Button button9 = findViewById(R.id.button9);
+        Button button10 = findViewById(R.id.button10);
+
+
+        button2.setOnClickListener(new View.OnClickListener()){
+            @Override
+            public void onClick(View v){
+
+            }
+        }
+
         arrowBack.setOnClickListener(new View.OnClickListener()
         {
             @Override
