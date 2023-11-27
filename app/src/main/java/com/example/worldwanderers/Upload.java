6a105be3a11c319fcba425b29af4c36e34d0589c
fragos5 <mitsosfragos@gmail.com>
2023-11-27 22:46:57 +0200

    feat: Image uploaded-Manually info input issue #1
    Signed-off-by: Roufria <wtfgreece@yahoo.gr>

--- app/src/main/java/com/example/worldwanderers/Upload.java
+++ app/src/main/java/com/example/worldwanderers/Upload.java
@@ -3,17 +3,29 @@ package com.example.worldwanderers;
 public class Upload {
     private String name;
     private String imageUrl;
-// ToDo: tha mpoun ki alla private pragmata px sintetagmenes tags ktlp
+    private String location;
+    private String date;
+    private String tag;
 
-    public Upload() {
-        //empty
-    }
-    public Upload(String name, String imageUrl) {
+
+    public Upload(String name, String imageUrl, String location,String date, String tag) {
         if (name.trim().equals("")) {
             name="No name";
         }
+        if (location.trim().equals("")){
+            location="No location";
+        }
+        if (location.trim().equals("")){
+            tag="No tag";
+        }
+        if (date.trim().equals("")){
+            date="No date";
+        }
         this.name = name;
         this.imageUrl = imageUrl;
+        this.location = location;
+        this.tag = tag;
+        this.date = date;
     }
 
     public String getName() {
@@ -30,6 +42,26 @@ public class Upload {
 
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
     }
 }
 
