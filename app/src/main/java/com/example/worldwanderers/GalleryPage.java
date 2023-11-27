5af9f2c1cd50b1e6a453f7757ea6a56bdd7eb04c
pavlgkai <gpaulos2002@gmail.com>
2023-11-27 18:48:52 +0200

    gallery update

--- app/src/main/java/com/example/worldwanderers/GalleryPage.java
+++ app/src/main/java/com/example/worldwanderers/GalleryPage.java
@@ -5,40 +5,51 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GalleryPage extends AppCompatActivity {
 
+    private View locationEditText,textView4,textView5,textView6,textView7,textView8,textView9,textView10,textView11,textView12;
+
+    private Button button2, button3,button4,button5,button6,button7,button8,button9,button10;
+
     @SuppressLint("MissingInflatedId")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.gallery);
+        locationEditText = findViewById(R.id.location_text);
+        textView4 = findViewById(R.id.textView4);
+        textView5 = findViewById(R.id.textView5);
+        textView6 = findViewById(R.id.textView6);
+        textView7 = findViewById(R.id.textView7);
+        textView8 = findViewById(R.id.textView8);
+        textView9 = findViewById(R.id.textView9);
+        textView10 = findViewById(R.id.textView10);
+        textView11 = findViewById(R.id.textView11);
+        textView12 = findViewById(R.id.textView12);
+
 
         ImageView arrowBack;
+
 
         arrowBack=findViewById(R.id.arrowBack);
 
         //buttons sto gallery
-        Button button2 = findViewById(R.id.button2);
-        Button button3 = findViewById(R.id.button3);
-        Button button4 = findViewById(R.id.button4);
-        Button button5 = findViewById(R.id.button5);
-        Button button6 = findViewById(R.id.button6);
-        Button button7 = findViewById(R.id.button7);
-        Button button8 = findViewById(R.id.button8);
-        Button button9 = findViewById(R.id.button9);
-        Button button10 = findViewById(R.id.button10);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
+        button6 = findViewById(R.id.button6);
+        button7 = findViewById(R.id.button7);
+        button8 = findViewById(R.id.button8);
+        button9 = findViewById(R.id.button9);
+        button10 = findViewById(R.id.button10);
 
 
-        button2.setOnClickListener(new View.OnClickListener()){
-            @Override
-            public void onClick(View v){
-
-            }
-        }
 
         arrowBack.setOnClickListener(new View.OnClickListener()
         {
