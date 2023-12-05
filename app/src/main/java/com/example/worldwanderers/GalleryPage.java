acf23ca6810ba5183e53f877de6db345fce3064f
pavlgkai <gpaulos2002@gmail.com>
2023-12-05 15:35:15 +0200

    gallery page changes

--- app/src/main/java/com/example/worldwanderers/GalleryPage.java
+++ app/src/main/java/com/example/worldwanderers/GalleryPage.java
@@ -4,57 +4,61 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-
+import android.widget.ListView;
+import android.widget.SearchView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GalleryPage extends AppCompatActivity {
     private EditText location_text;
+    ListView listView;
+    String[] name = {"Mountains", "Sea", "Friends", "Family", "Sports", "Art"};
 
-    @SuppressLint("MissingInflatedId")
-    protected void onCreate(Bundle savedInstanceState) {
+    ArrayAdapter<String> arrayAdapter;
+    @Override
+    public boolean onCreateOptionMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView menuItem.getActionView();
+        SearchView.setQueryHint("Type here to search");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                  arrayAdapter.getFilter().filter(newText);
+
+                return false;
+
+            }
+        });
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.gallery);
+          ListView = findViewById(R.id.listview);
+
+        arrayAdapter = new ArrayAdapter<String>( context: this, android.R.layout.simple_list_item_1,name);
+        listView.setAdapter(arrayAdapter);
+
 
         ImageView arrowBack;
 
         arrowBack = findViewById(R.id.arrowBack);
 
-        //buttons sto gallery
-        Button button2 = findViewById(R.id.button2);
-        Button button3 = findViewById(R.id.button3);
-        Button button4 = findViewById(R.id.button4);
-        Button button5 = findViewById(R.id.button5);
-        Button button6 = findViewById(R.id.button6);
-        Button button7 = findViewById(R.id.button7);
-        Button button8 = findViewById(R.id.button8);
-        Button button9 = findViewById(R.id.button9);
-        Button button10 = findViewById(R.id.button10);
-
-
-        location_text.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (TextUtils.isEmpty(location_text.getText())) {
-                    // Εάν το location EditText είναι ήδη κενό, μην κάνετε τίποτα
-                } else {
-                    // Κάντε ό,τι θέλετε όταν πατηθεί το EditText της τοποθεσίας
-                }
-            }
-        });
-        {
-       /*     button3.setOnClickListener(new View.OnClickListener() {
-        });
-            @Override
-            public void onClick(View v){
-
-            }
-        }*/
 
             arrowBack.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -66,6 +70,5 @@ public class GalleryPage extends AppCompatActivity {
             });
 
         }
-    }
+    } }
 }
-
